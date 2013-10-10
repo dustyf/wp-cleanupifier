00f24d1ac71d856073e4b417fb09f9c152c6a80e
Dustin Filippini <dusty@dustyf.com>
2013-10-10 15:25:29 -0500

    Namespacing ALL the Functions

--- wp-cleanupifier.php
+++ wp-cleanupifier.php
@@ -13,20 +13,18 @@ License: GPL2
 /**
  * WP Head Cleaner
  */
-function removeHeadLinks() {
-   	remove_action('wp_head', 'rsd_link');
-   	remove_action('wp_head', 'wlwmanifest_link');
-   	remove_action('wp_head', 'wp_generator');
+add_action( 'init', 'wpc_remove_head_links' );
+function wpc_remove_head_links() {
+   	remove_action( 'wp_head', 'rsd_link' );
+   	remove_action( 'wp_head', 'wlwmanifest_link' );
+   	remove_action( 'wp_head', 'wp_generator' );
 }
-add_action('init', 'removeHeadLinks');
 
-add_action( 'add_admin_bar_menus', 'remove_wp_admin_bar_wp_menu', 1 );
 /**
  * Remove WP Admin Bar WP Menu
- * 
- * @return null void
  */
-function remove_wp_admin_bar_wp_menu() {
+add_action( 'add_admin_bar_menus', 'wpc_remove_wp_admin_bar_wp_menu', 1 );
+function wpc_remove_wp_admin_bar_wp_menu() {
   remove_action( 'admin_bar_menu', 'wp_admin_bar_wp_menu', 10 );
 }
 
@@ -34,93 +32,92 @@ function remove_wp_admin_bar_wp_menu() {
  * To Disable RSS Feeds, uncomment the below lines
  */
 
-// function disable_all_feeds() {
-//    wp_die( __('Sorry, our content is not available by RSS. Please head over to <a href="'. get_bloginfo('url') .'">our site</a>') );
+// function wpc_disable_all_feeds() {
+//    wp_die( __( 'Sorry, our content is not available by RSS. Please head over to <a href="'. get_bloginfo('url') .'">our site</a>' ) );
 // }
 
-//add_action('do_feed', 'disable_all_feeds', 1);
-//add_action('do_feed_rdf', 'disable_all_feeds', 1);
-//add_action('do_feed_rss', 'disable_all_feeds', 1);
-//add_action('do_feed_rss2', 'disable_all_feeds', 1);
-//add_action('do_feed_atom', 'disable_all_feeds', 1);
+//add_action( 'do_feed', 'wpc_disable_all_feeds', 1) ;
+//add_action( 'do_feed_rdf', 'wpc_disable_all_feeds', 1 );
+//add_action( 'do_feed_rss', 'wpc_disable_all_feeds', 1 );
+//add_action( 'do_feed_rss2', 'wpc_disable_all_feeds', 1 );
+//add_action( 'do_feed_atom', 'wpc_disable_all_feeds', 1) ;
 //remove_action( 'wp_head', 'feed_links_extra', 3 ); 
 //remove_action( 'wp_head', 'feed_links', 2 );
 
 /**
  * Disable theme file editing
  */
-define('DISALLOW_FILE_EDIT',true);
+define( 'DISALLOW_FILE_EDIT', true );
 
 
 /**
  * Remove Dashboard Widgets
  */
-add_action('admin_init', 'rw_remove_dashboard_widgets');
-function rw_remove_dashboard_widgets() {
-	remove_meta_box('dashboard_right_now', 'dashboard', 'normal');   // right now
-	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); // recent comments
-	remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');  // incoming links
-	remove_meta_box('dashboard_plugins', 'dashboard', 'normal');   // plugins
-	remove_meta_box('dashboard_quick_press', 'dashboard', 'normal');  // quick press
-	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'normal');  // recent drafts
-	remove_meta_box('dashboard_primary', 'dashboard', 'normal');   // wordpress blog
-	remove_meta_box('dashboard_secondary', 'dashboard', 'normal');   // other wordpress news
+add_action( 'admin_init', 'wpc__remove_dashboard_widgets' );
+function wpc__remove_dashboard_widgets() {
+	remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );   // right now
+	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' ); // recent comments
+	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal') ;  // incoming links
+	remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );   // plugins
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'normal' );  // quick press
+	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'normal' );  // recent drafts
+	remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );   // wordpress blog
+	remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );   // other wordpress news
 }
-add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
+add_action( 'wp_dashboard_setup', 'wpc_remove_dashboard_widgets' );
 
 /**
  * Remove Screen Options
  */
-// function remove_screen_options_tab()
+// function wpc_remove_screen_options_tab()
 // {
 //     return false;
 // }
-// add_filter('screen_options_show_screen', 'remove_screen_options_tab');
+// add_filter( 'screen_options_show_screen', 'wpc_remove_screen_options_tab' );
  
  /**
  * Remove Help Tab
  */
-function hide_help() {
+function wpc_hide_help() {
     echo '<style type="text/css">
            #contextual-help-link-wrap { display: none !important; }
          </style>';
 }
-add_action('admin_head', 'hide_help');
+add_action( 'admin_head', 'wpc_hide_help' );
 
-function my_custom_dashboard_widgets() {
+function wpc_custom_dashboard_widgets() {
 global $wp_meta_boxes;
  
-wp_add_dashboard_widget('custom_help_widget', 'My Widget Title', 'custom_dashboard_help');
+wp_add_dashboard_widget( 'custom_help_widget', 'My Widget Title', 'wpc_custom_dashboard_help' );
 }
 
  /**
  * Create a custom dashboard widget
  */
-function custom_dashboard_help() {
+function wpc_custom_dashboard_help() {
 echo 'My widget content';
 }
- 
-add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+add_action( 'wp_dashboard_setup', 'wpc_custom_dashboard_widgets' );
 
 /**
  * Filter Yoast Meta Priority
  **/
-function gf_wpseo_metabox_prio() {
+function wpc_wpseo_metabox_prio() {
 	return 'low' ;								  
 }
-add_filter('wpseo_metabox_prio' , 'gf_wpseo_metabox_prio' );
+add_filter( 'wpseo_metabox_prio' , 'wpc_wpseo_metabox_prio' );
 
 /**
  * Remove AIM, Jabber, YIM from User Profile
  **/
-if ( !function_exists( 'extra_contact_info' ) ) :
-	function extra_contact_info($contactmethods) {
+if ( ! function_exists( 'wpc_extra_contact_info' ) ) :
+	function wpc_extra_contact_info( $contactmethods ) {
 	
-	    unset($contactmethods['aim']);
-	    unset($contactmethods['jabber']);
-	    unset($contactmethods['yim']);
+	    unset( $contactmethods['aim'] );
+	    unset( $contactmethods['jabber'] );
+	    unset( $contactmethods['yim'] );
 	
 	    return $contactmethods;
 	}
-	add_filter('user_contactmethods', 'extra_contact_info');
+	add_filter( 'user_contactmethods', 'wpc_extra_contact_info' );
 endif;
